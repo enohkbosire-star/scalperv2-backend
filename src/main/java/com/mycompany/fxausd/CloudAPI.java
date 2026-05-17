3bc6edddfde124096194e5f4034e1a1f07bb9663
PC <PC@DESKTOP-2VPR9IB>
2026-05-17 16:40:53 +0300

    fix the final update

--- src/main/java/com/mycompany/fxausd/CloudAPI.java
+++ src/main/java/com/mycompany/fxausd/CloudAPI.java
@@ -733,11 +733,9 @@ public class CloudAPI {
     private static void sendEmail(String recipient, String subject, String content) throws Exception {
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "465");
-        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.port", "587");
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
         props.put("mail.smtp.connectiontimeout", "15000");
         props.put("mail.smtp.timeout", "15000");
